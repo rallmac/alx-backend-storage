087f5b18da7a09607f8a587f36d557961a6db102
rallmac <tobijahekperikpe@gmail.com>
2024-10-17 20:17:16 +0100

    task 1 redo

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,11 +1,12 @@
 --This script creates a table called users
 --it drops the table if it already exits
+
+
 DROP TABLE IF EXISTS users;
 
-
 CREATE TABLE IF NOT EXISTS users (
-	id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-	email VARCHAR(255) NOT NULL UNIQUE,
-	name VARCHAR(255),
-	country ENUM('US', 'CO', 'TN') DEFAULT 'US' NOT NULL
+    id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    country ENUM('US', 'CO', 'TN') DEFAULT 'US' NOT NULL
 );
