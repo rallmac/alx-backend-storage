78fe94c3003eaf75c5b4c720ba7824bf7fb95190
rallmac <tobijahekperikpe@gmail.com>
2024-10-17 17:51:07 +0100

    task 10

---
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -0,0 +1,18 @@
+-- This creates a function SafeDiv divides and returns
+-- Second number equals to zero
+
+DELIMITER $$
+
+CREATE FUNCTION SafeDiv(a INT, b INT)
+RETURNS DECIMAL(10, 2)
+DETERMINISTIC
+BEGIN
+    -- Return 0 if b is 0, otherwise return the result of a / b
+    IF b = 0 THEN
+        RETURN 0;
+    ELSE
+        RETURN a / b;
+    END IF;
+END$$
+
+DELIMITER ;
