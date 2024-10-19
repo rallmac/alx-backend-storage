6cbc98e3a75068c1dd25ade41025f36001f211cf
rallmac <tobijahekperikpe@gmail.com>
2024-10-20 00:30:27 +0100

    task 5

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -4,15 +4,12 @@
 DELIMITER $$
 
 CREATE TRIGGER reset_valid_email
-AFTER UPDATE ON users
+BEFORE UPDATE ON users
 FOR EACH ROW
 BEGIN
     -- Check if the email has been changed
     IF NEW.email <> OLD.email THEN
-        -- Reset the valid_email attribute
-        UPDATE users
-        SET valid_email = FALSE
-        WHERE id = NEW.id;
+        SET NEW.valid_email = 0;
     END IF;
 END$$
 
