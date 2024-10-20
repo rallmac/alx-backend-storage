f15948f3929d616ab58e970a4c38f594d7e6cea3
rallmac <tobijahekperikpe@gmail.com>
2024-10-20 01:02:46 +0100

    task 7

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,14 +1,12 @@
 -- This script creates a stored procedure which computes and
 -- stores average score for students. Average score can be a decimal
 
-
 DELIMITER $$
 
 CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 BEGIN
     DECLARE avg_score FLOAT;
 
-    -- Compute the average score for the given user
     SELECT AVG(score) INTO avg_score
     FROM corrections
     WHERE user_id = user_id;
