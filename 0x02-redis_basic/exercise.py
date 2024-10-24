429d99bac46d1b94cacd52b347829521be49f644
rallmac <tobijahekperikpe@gmail.com>
2024-10-24 02:43:54 +0100

    task 1

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -3,11 +3,11 @@
 
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable, Optional
 
 
 class Cache:
-    """Here the class is being declared
+    """Cache class for storing data in Redis.
     """
     def __init__(self):
         """Initialize the Cache class.
@@ -22,3 +22,26 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(
+            self,
+            key: str,
+            fn: Optional[Callable] = None
+            ) -> Union[str, bytes, int, float, None]:
+        """Retrieve data from Redis and optionally convert
+           it using a callable function.
+        """
+        data = self._redis.get(key)
+        if data is None:
+            return None
+        if fn:
+            return fn(data)
+        return data
+
+    def get_str(self, key: str) -> Optional[str]:
+        """Retrieve data as a UTF-8 decoded string."""
+        return self.get(key, lambda d: d.decode('utf-8'))
+
+    def get_int(self, key: str) -> Optional[int]:
+        """Retrieve data as an integer."""
+        return self.get(key, lambda d: int(d))
