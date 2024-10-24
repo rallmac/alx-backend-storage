762013bb97dde71a4c3e37539ac755b556568216
rallmac <tobijahekperikpe@gmail.com>
2024-10-24 04:01:37 +0100

    task2

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,61 +4,61 @@
 """
 
 import redis
-import uuid
-from typing import Union, Callable, Optional
+from uuid import uuid4
+from typing import Any, Union, Callable, Optional
 from functools import wraps
-
-
-def count_calls(method: Callable) -> Callable:
-    """Decorator to count how many times a method is called."""
-    @wraps(method)
-    def wrapper(self, *args, **kwargs):
-        """Wrapper function to increment the call count in Redis.
-        """
-        key = f"count:{method.__qualname__}"
-        self._redis.incr(key)
-        return method(self, *args, **kwargs)
-
-    return wrapper
 
 
 class Cache:
     """Cache class for storing data in Redis.
     """
-    def __init__(self):
+    def __init__(self) -> None:
         """Initialize the Cache class.
         """
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """Store the data in Redis with a random key and
            return the key.
         """
-        key = str(uuid.uuid4())
-        self._redis.set(key, data)
+        key = str(uuid4())
+        client = self._redis
+        client.set(key, data)
         return key
 
-    def get(
-            self,
-            key: str,
-            fn: Optional[Callable] = None
-            ) -> Union[str, bytes, int, float, None]:
+    def get(self, key: str, fn: Optional[Callable] = None) -> Any:
         """Retrieve data from Redis and optionally convert
            it using a callable function.
         """
-        data = self._redis.get(key)
-        if data is None:
-            return None
-        if fn:
-            return fn(data)
-        return data
+        client = self._redis
+        value = client.get(key)
+        if not value:
+            return
+        if fn is int:
+            return self.get_int(value)
+        if fn is str:
+            return self.get_str(value)
+        if callable(fn):
+            return fn(value)
+        return value
 
-    def get_str(self, key: str) -> Optional[str]:
+    def get_str(self, data: bytes) -> str:
         """Retrieve data as a UTF-8 decoded string."""
-        return self.get(key, lambda d: d.decode('utf-8'))
+        return data.decode("utf-8")
 
-    def get_int(self, key: str) -> Optional[int]:
+    def get_int(self, data: bytes) -> int:
         """Retrieve data as an integer."""
-        return self.get(key, lambda d: int(d))
+        return int(data)
+
+
+def count_calls(method: Callable) -> Callable:
+    """Decorator to count how many times a method is called."""
+    @wraps(method)
+    def wrapper(self: Any, *args, **kwargs) -> str:
+        """Wrapper function to increment the call count
+           in Redis.
+        """
+        self._redis.incr(method.__qualname__)
+        return method(self, *args, **kwargs)
+    return wrapper
